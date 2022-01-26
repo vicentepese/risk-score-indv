2896a01a41e24646a8d25f3fd774c7eff46cc3f2
vicentepese <vsempereperis@gmail.com>
2022-01-25 22:45:39 -0800

    test_version

--- calc_risk_score.py
+++ calc_risk_score.py
@@ -17,6 +17,17 @@ def main():
     process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
     output, error = process.communicate()
 
+    # Get risk score of patients with phenotype 
+    pheno = pd.read_table(opts['file']['pheno'], sep =' ', header=None)
+    pheno.columns = ['FID','IID', 'pheno']
+    prs = pd.read_table(opts['file']['prs'], sep = ' ')
+
+    # Filter 
+    prs_filt = prs[prs.FID.isin(pheno['FID'])]
+
+    # Write 
+    prs_filt.to_csv('prs.csv', sep = ' ', header=True, index=False)
+
 
 
 
--- utils/calc_rs_ignore_amb.sh
+++ utils/calc_rs_ignore_amb.sh
@@ -6,7 +6,7 @@ module load r
 Rscript PRSice.R --dir /home/vipese/Documents/risk-score-indv/ \
     --prsice /home/vipese/Documents/risk-score-indv/PRSice_linux \
     --pheno /home/vipese/Documents/risk-score-indv/res/pheno.txt \
-    --base res/didriksen_snp.csv --snp SNP --chr CHR --bp POS --A1 EA --A2 OA --stat OR --pvalue PVAL\
+    --base /home/vipese/Documents/risk-score-indv/res/didrksen_MEIS_BTBD.csv --snp SNP --chr CHR --bp POS --A1 EA --A2 OA --stat OR --pvalue PVAL\
     --target /labs/mignot/RLS_RS/IMPUTED_BY_CHR/RLS_GWAS \
     --thread 1 \
     --stat OR \
