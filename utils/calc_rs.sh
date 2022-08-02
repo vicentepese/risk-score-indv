3437977dbfb0dad68edb61643391c6e1578c37df
vicentepese <vsempereperis@gmail.com>
2022-08-02 00:29:19 -0700

    bash compute RS

--- utils/calc_rs.sh
+++ utils/calc_rs.sh
@@ -6,8 +6,10 @@ module load r
 Rscript PRSice.R --dir /home/vipese/Documents/risk-score-indv/ \
     --prsice /home/vipese/Documents/risk-score-indv/PRSice_linux \
     --pheno /home/vipese/Documents/risk-score-indv/res/pheno.txt \
-    --base /home/vipese/Documents/risk-score-indv/res/didrksen_MEIS_BTBD.csv --snp SNP --chr CHR --bp POS --A1 EA --A2 OA --stat OR --pvalue PVAL\
+    --base /home/vipese/Documents/risk-score-indv/res/didriksen_snp.csv --snp SNP --chr CHR --bp POS --A1 EA --A2 OA --stat OR --pvalue PVAL\
     --target /labs/mignot/RLS_RS/IMPUTED_BY_CHR/RLS_GWAS \
+    --cov /home/vipese/Documents/risk-score-indv/res/PCA_header.eigenvec --cov-col @PC[1-5]
+    --all-score \
     --keep-ambig \
     --thread 1 \
     --stat OR \
